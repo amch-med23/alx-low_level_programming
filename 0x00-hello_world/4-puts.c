0949e9a35d1c42b61edc22c78df12741b25d75fa
amch-med23 <skyied.med2023@gmail.com>
2023-06-15 23:20:49 +0000

    adding the description and puting the output directelly to the puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+/*
+* this is the main function
+*/
+
 int main(void)
 {
 
-  char Str[] = "\"Programming is like building a multilingual puzzle\n";
-
-  puts(Str);
+  puts("\"Programming is like building a multilingual puzzle\n");
 
   return (0);
 }
