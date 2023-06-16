7e38615d8e386f224089380667e280db70ac83eb
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 16:50:52 +0000

    adding the space befour the '(' for the while loop.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	char alphabets = 'a';
 
-	while(alphabets <= 'z')
+	while (alphabets <= 'z')
 	{
 		putchar(alphabets);
 		alphabets++;
 	}
 	alphabets = 'A';
-	while(alphabets <= 'Z')
+	while (alphabets <= 'Z')
 	{
 		putchar(alphabets);
 		alphabets++;
