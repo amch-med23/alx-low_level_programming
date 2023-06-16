ed75bf5a503f4a958cb3053c361b7df172349c73
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 16:49:24 +0000

    creting the file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - It is the main function in the program.
+ * Return: it return (0), Success.
+ */
+
+int main(void)
+{
+	char alphabets = 'a';
+
+	while(alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	alphabets = 'A';
+	while(alphabets <= 'Z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	putchar('\n');
+	return (0);
+}
