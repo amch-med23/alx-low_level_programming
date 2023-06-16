fde176a527ff6717a35bb1215419ea923dd3dd49
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 22:23:15 +0000

    creating the file 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main -it's the main function of every program.
+ * Return: it's always returning (0), Success
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = 49; b <= 56; b++)
+		{
+			for (c = 50; c <= 57; c++)
+			{			       
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56 || c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
