5fdd10064cda7679db47a5b55803c7a28d6b2cfa
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 14:36:52 +0000

    certing the file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - the main function in the program.
+ * Description: the main function is the function where
+ * the program starts from.
+ * Return: it return the value of the string.
+ */
+
+int main(void)
+{
+        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
