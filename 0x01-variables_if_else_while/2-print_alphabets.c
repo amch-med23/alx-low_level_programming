c6f2e9248d7846cabaac06da8c400499b8b5e160
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 16:27:46 +0000

    creating the file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - It is the main function.
+ *Return: it return (0)
+ */
+
+int main(void)
+{
+	char alphabet;
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
