f5471e24cf922b9f07c93987a8e2e758dda08355
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 17:25:55 +0000

    creating the file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - It's the main function in a program.
+ * Return: It's always (0), means Success.
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while( ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
