304f37ff45bf8d3ff2bf4a1db849c97d611505f4
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 17:21:47 +0000

    creating the file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the main function of every program.
+ * Return: I's always (0), it means Success.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
