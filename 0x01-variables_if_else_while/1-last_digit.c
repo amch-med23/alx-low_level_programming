948b7f3dd56e171c6c9200c6e60e31b2ca08acb2
amch-med23 <skyied.med2023@gmail.com>
2023-06-16 15:22:14 +0000

    fixing the == egality check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	/* your code goes there */
 	if (n > 5)
 		printf("Last digit of %d is greater than 5\n", n);
-	else if (n = 0)
+	else if (n == 0)
 		printf("Last digit of %d is 0\n", n);
 	else
 		printf("Last digit of %d is less than 6 and not 0\n", n);
