21048dd760bf61ab93181a08f401f25d324a7db2
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 16:08:41 +0000

    commenting the function _putchar in main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,9 @@ int putchar(int j);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _putchar(int c)
+/*int _putchar(int c)
 {
 	int putchar(int k);
 	return (putchar(c));
 }
+*/
