8b8be07028f317c9bf8027cc9e15f573aba7906a
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 21:29:16 +0000

    changing the operation inside _putchar() from substruction to addition

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -20,7 +20,7 @@ int print_last_digit(int i)
 		b = i * (-1);
 		result = b % 10;
 		cc = (int) (ch);
-		return (_putchar(result - cc));
+		return (_putchar(result + cc));
 	}
 	else
 	{
@@ -29,6 +29,6 @@ int print_last_digit(int i)
 
 		cc = (int) (ch);
 		result = i % 10;
-		return (_putchar(result - cc));
+		return (_putchar(result + cc));
 	}
 }
