681b7e82f7d953c66831e2d572b15d81056aed37
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 14:09:25 +0000

    creating a function inside the main.h file called 'print_alphabet()' that prints all the alphabets in lowercase, and creating the file 1-alphabet that uses that function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - the entry point.
+ * Return: It's (0).
+ * print_alphabet - prints the alphabets from a to z in lowercase.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,14 @@
 int putchar(int ch);
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return;
+}
