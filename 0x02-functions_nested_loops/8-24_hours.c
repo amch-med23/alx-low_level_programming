b979e0b433611bae1554a875a7ddc81f52294015
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 23:49:12 +0000

    using if statements to change where the h2 will count to 9 and where it will count to 3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,28 +8,47 @@ void jack_bauer(void)
 {
 	int h1, h2, m1, m2;
 	int h2t;
-	bool c;
 
+	h2 = 0;
 	h2t = 1;
 	for (h1 = 0; h1 <= 2; h1++)
 	{
-		
-		(h2t <= 2) ? c = (h2 <= 9) : c = (h2 <= 3);
-		for (h2 = 0; c; h2++)
+		if (h2t <= 2)
 		{
-			for (m1 = 0; m1 <= 5; m1++)
+			while (h2 <= 9)
 			{
-				for (m2 = 0; m2 <= 9; m2++)
+				for (m1 = 0; m1 <= 5; m1++)
 				{
-					_putchar(h1 + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(m1 + '0');
-					_putchar(m2 + '0');
-					_putchar('\n');
+					for (m2 = 0; m2 <= 9; m2++)
+					{
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
+						_putchar(':');
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
+						_putchar('\n');
+					}
+				}
+				h2++;
+				h2t++;
+			}
+		}
+		else
+		{
+			while (h2 <= 3)
+			{
+				for (m1 = 0; m1 <= 5; m1++)
+				{
+					for (m2 = 0; m2 <= 9; m2++)
+					{
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
+						_putchar(':');
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
+					}
 				}
 			}
-			h2t++;
 		}
 	}
 }
