ec8e2c16ebd3ba900753fac0ea77a11171b75070
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 15:01:40 +0000

    adding the description of the function, and reinitializing the character value to 'a' each time the loop starts

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,18 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - ptints 10 times the alphabets from 'a' to 'z'
+ */
 void print_alphabet_x10(void)
 {
 	int i;
 	char c;
 
 	i = 0;
-	c = 'a';
 	while (i < 10)
 	{
-		while(c <= 'z')
+		while (c <= 'z')
 		{
+			c = 'a';
 			putchar(c);
 			c++;
 		}
