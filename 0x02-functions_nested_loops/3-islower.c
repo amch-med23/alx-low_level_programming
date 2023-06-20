52718c70a537194994c6c348dafc6caec1b6bca3
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 18:33:00 +0000

    adding description to the parameter c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * _islower - checks is a haracter is lowercase or not.
+ * @c: the character that's gonna be passed to the function to be checked.
+ *
  * Return: it's (0) for lowercase, and (1) for uppercase.
  */
 
