b0217e110a2423b16e49d8277b2d1f2cb8ddb917
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 15:54:35 +0000

    adding the _putcahr definition to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,9 @@
-int putchar( int j);
+int putchar(int j);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _putchar(int c)
+{
+	int putchar(int k);
+	return (putchar(c));
+}
