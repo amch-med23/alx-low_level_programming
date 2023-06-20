4ab56be5500e1c377fbc1153cd6f766cc54a404b
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 14:52:29 +0000

    adding the declaration of the function print_alphabet_x10() to the mian.h, and creating the function itselfe in the file 2-alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = 'a';
+	while (i < 10)
+	{
+		while(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putcahr('\n');
+		i++;
+	}
+}
