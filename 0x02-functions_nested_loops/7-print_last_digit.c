51268fcee0fb894044f224b824de846e6cf91bc0
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 21:10:27 +0000

    returning the value of the _putcahr(result + '0')

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,21 +9,19 @@
 int print_last_digit(int i)
 {
 	int result;
-	int k;
+
 
 	if (i < 0)
 	{
 		int b;
-		
+
 		b = i * -1;
 		result = b % 10;
-		k = _putchar(result);
-		return (k);
+		return (_putchar(result + '0'));
 	}
 	else
 	{
 		result = i % 10;
-		k = _putchar(result);
-		return (k);
+		return (_puchar(result + '0'));
 	}
 }
