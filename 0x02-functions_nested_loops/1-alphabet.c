9f91513099a08d4317ec279ee8cf28de8ecb449e
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 14:18:27 +0000

    removing the function declaration from the file 1-alphabet.c and declare it in main.h, but the function definition is still in the file 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,22 +5,22 @@
  * print_alphabet - prints the alphabets from a to z in lowercase.
  */
 /* declaring the function print_alphabet */
-void print_alphabet(void);
+/*void print_alphabet(void);*/
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
-void print_alphabet(void)  
-{			   
-	char c;		   
-			   
-	c = 'a';	   
-	while (c <= 'z')   
-	{		   
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
 		putchar(c);
-		c++;	   
-	}		   
-	putchar('\n');	   
-	return;		   
+		c++;
+	}
+	putchar('\n');
+	return;
 }
