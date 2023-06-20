a91cf36c44e3a14a0d43f4cd250799a506a516db
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 23:25:18 +0000

    addig the variable h2t to control how many times h2 will be counting to 9 then to 3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-o#include "main.h"
+#include "main.h"
 /**
  * jack_bauer - prints all the minutes in a day.
  * Return: it's not useful because the function has a void return type.
@@ -9,12 +9,13 @@ void jack_bauer(void)
 	int h1, h2, m1, m2;
 	int h2t;
 
-	h2t = 0;
+	h2t = 1;
 	for (h1 = 0; h1 <= 2; h1++)
 	{
-		for (h2 = 0; h2 <= 9 && h2t < 3; h2++)
+		
+		h2t <= 2 ? c = (h2 <= 9) : c = (h2 <= 3);
+		for (h2 = 0; c; h2++)
 		{
-
 			for (m1 = 0; m1 <= 5; m1++)
 			{
 				for (m2 = 0; m2 <= 9; m2++)
