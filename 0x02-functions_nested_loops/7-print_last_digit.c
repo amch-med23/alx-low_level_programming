80655af5b794f2826057db63098455db55612513
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 21:27:40 +0000

    using typecasting to convert the value of the char '0' to integer, and then substruct it from the value of the result

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,14 +14,21 @@ int print_last_digit(int i)
 	if (i < 0)
 	{
 		int b;
+		int cc;
+		char ch = '0';
 
 		b = i * (-1);
 		result = b % 10;
-		return (_putchar(result + '0' + '0'));
+		cc = (int) (ch);
+		return (_putchar(result - cc));
 	}
 	else
 	{
+		int cc;
+		char ch = '0';
+
+		cc = (int) (ch);
 		result = i % 10;
-		return (_putchar(result + '0' + '0'));
+		return (_putchar(result - cc));
 	}
 }
