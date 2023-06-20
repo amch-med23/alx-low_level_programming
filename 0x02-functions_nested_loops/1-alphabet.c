0fd7123895a418489332b89cec2ab8b2e3f9b900
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 14:30:38 +0000

    removing the trailing whitespace from line 4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet - prints the alphabets from a to z in lowercase.
- * Description: this function uses two putchar functions to print 
+ * Description: this function uses two putchar functions to print
  * alphabets from 'a' to 'z'.
  */
 /*the function to print the alphabets*/
