89fa29b38161f2366dacbe034ca1de49dcf9062d
amch-med23 <skyied.med2023@gmail.com>
2023-06-20 14:14:36 +0000

    removing the function defination from main.h and define it in 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,23 @@
  * Return: It's (0).
  * print_alphabet - prints the alphabets from a to z in lowercase.
  */
-
+/* declaring the function print_alphabet */
+void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+void print_alphabet(void)  
+{			   
+	char c;		   
+			   
+	c = 'a';	   
+	while (c <= 'z')   
+	{		   
+		putchar(c);
+		c++;	   
+	}		   
+	putchar('\n');	   
+	return;		   
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,1 @@
 int putchar(int ch);
-void print_alphabet(void)
-{
-	char c;
-
-	c = 'a';
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	putchar('\n');
-	return;
-}
