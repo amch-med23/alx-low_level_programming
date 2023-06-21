2cac3b036e3ee72cda26e54aaeedcdd6fd358e70
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 13:59:06 +0000

    changing the complete methode to for loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,76 +6,32 @@
 
 void times_table(void)
 {
-	int i;
-	int k;
-	int z;
-	int res;
-	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int m, n, l;
 
-	i = 0;
-	while (i < 10)
+	for (m = 0; m < 10; m++)
 	{
-		k = 0;
-		res = 0;
-		z = 0;
-		while (k < 10)
+		for (n = 0; n < 10; n++)
 		{
-			res = arr[i] * arr[k];
-			if (k < 9 && i < 10)
+			l = n * m;
+			if (m == 0)
 			{
-				if (res >= 10)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-					_putchar(',');
-				}
-				else if (res == 0 && z < 1)
-				{
-					_putchar(res + '0');
-					_putchar(',');
-				}
-				else if (res == 0 && z >= 1)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(res + '0');
-					_putchar(',');
-				}
-				else if (res > 0 && res < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(res + '0');
-					_putchar(',');
-				}
+				_putchar(l + '0');
 			}
-			else
-			{
-				if (res != 0)
-				{
-					int a, b;
 
-					a = res / 10;
-					b = res % 10;
-					if (a != b)
-					{
-						_putchar(' ');
-						_putchar(a + '0');
-						_putchar(b + '0');
-					}
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(res + '0');
-				}
+			if (l < 10 && n != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(l + '0');
+			} else if (l >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((l / 10) + '0');
+				_putchar((l % 10) + '0');
 			}
-			k++;
-			z++;
 		}
-		i++;
 		_putchar('\n');
 	}
 }
