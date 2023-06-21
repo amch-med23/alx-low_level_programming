12d8b9a6e7726f0e4d5b385a0e4faf3bc1c51fa8
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 18:31:19 +0000

    creating the file 102-fibonacci.c to print the first 50 fibonacci numbers starting from 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the entry point.
+ * Return: it's (0), Success.
+ */
+
+int main(void)
+{
+	int a, prev, i;
+
+	b = 0;
+	prev = b;
+	for (i = 1; i <= 50; i++);
+	{
+		a = prev + i;
+		printf("%d, ", a);
+		prev = a;
+	}
+	return (0);
+}
