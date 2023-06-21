1edc097a0c6e7345d787103c6d530a1dfa2d68b9
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 15:42:31 +0000

    fixing the space befour ')'

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,16 +6,16 @@
 
 void print_times_table(int n)
 {
+	int i, j, k;
+	
 	if (n >= 0 && n <= 15)
 	{
-		int i, j, k;
-
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
-				if (j == 0 )
+				if (j == 0)
 				{
 					_putchar(k + '0');
 				}
@@ -40,7 +40,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar(((k / 10 ) % 10) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
