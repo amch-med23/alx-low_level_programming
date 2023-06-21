0f92d8b5118d6a0b814014ff37105df4833361a6
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 12:02:52 +0000

    changing the paces and the commas positions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,22 +18,22 @@ void times_table(void)
 		res = 0;
 		while (k <= 10)
 		{
-			res = (arr[i] * arr[k]);
+			res = arr[i] * arr[k];
 			if (k < 9 && i < 9)
 			{
 				if (res >= 10)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
+					_putchar(',');
 				}
-				else if (res < 10)
+				else
 				{
 					_putchar(res + '0');
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(',')
 				}
 			}
 			else
