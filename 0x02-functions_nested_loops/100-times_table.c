0cba64671351f13af2a20215abb28480c026ad5e
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 15:20:59 +0000

    fixing the dentation error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,6 +22,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 				}
 				else if (k < 10 && j == n)
 				{
@@ -30,31 +31,30 @@ void print_times_table(int n)
 				}
 				else if (k >= 10 && k < 100 && j < n)
 				{
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				else if (k >= 10 && k < 100 && j == n)
 				{
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((k / 10)+ '0');
 					_putchar((k % 10) + '0');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar('\n');
 				}
 				else if (k >= 100 && j < n)
 				{
-					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10 ) % 10) + '0');
 					_putchar((k % 10) + '0');
 					_putchar(',');
+					_putchar(' ');
 				}
 				else if (k >= 100 && j == n)
 				{
-					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
