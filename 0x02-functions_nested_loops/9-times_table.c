d5fc85d4d2e6c18231a5120f71c4880930f55a45
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 12:09:02 +0000

    adding the division by 10 and the modulo by 10 to the last digit in the line, and fixing the spaces and the commas position

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,22 +23,23 @@ void times_table(void)
 			{
 				if (res >= 10)
 				{
-					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
 					_putchar(',');
+					_putchar(' ');
 				}
 				else
 				{
 					_putchar(res + '0');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 			}
 			else
 			{
-				_putchar(res + '0');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
 			k++;
 		}
