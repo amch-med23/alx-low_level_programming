55f4be8fe2d4605c052503fa77c5cd2e28060bbc
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 20:30:19 +0000

    creating the file 13-fibonacci.c that adds the sum of the even-valued terms of the fibonacci numbers less or equale to 4oooooo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - the Enrty point.
+ * Return: it's (0).
+ */
+
+int main(void)
+{
+	unsigned long int sum;
+	unsigned long int counter;
+	unsigned long int prev1, prev2;
+
+	prev1 = 0;
+	prev2 = 1;
+	for (counter = 1; counter <= 4000000; counter++)
+	{
+		sum = prev1 + prev2;
+		prev2 = prev1;
+		prev1 = sum;
+		int tmp;
+		int sumtmp;
+
+		tmp = sum % 2;
+		sumtmp = 0;
+		if(tmp != 0)
+		{
+			sumtmp += sum;
+		}
+	}
+	printf("%lu\n", sumtmp);
+	return (0);
+}
