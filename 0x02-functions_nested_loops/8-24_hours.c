538d6acb3f08b1f23e7903b3053badc1204bdfb5
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 00:02:13 +0000

    using a variable to represant the hours and another variable to represent the minutes, instead of four separate variables

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,49 +6,22 @@
 
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
-	int h2t;
+	int i, j;
 
-	h2 = 0;
-	h2t = 1;
-	for (h1 = 0; h1 <= 2; h1++)
+	i = 0;
+	j = 0;
+	while (i < 24)
 	{
-		if (h2t <= 2)
+		while (j < 60)
 		{
-			while (h2 <= 9)
-			{
-				for (m1 = 0; m1 <= 5; m1++)
-				{
-					for (m2 = 0; m2 <= 9; m2++)
-					{
-						_putchar(h1 + '0');
-						_putchar(h2 + '0');
-						_putchar(':');
-						_putchar(m1 + '0');
-						_putchar(m2 + '0');
-						_putchar('\n');
-					}
-				}
-				h2++;
-				h2t++;
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
-		else
-		{
-			while (h2 <= 3)
-			{
-				for (m1 = 0; m1 <= 5; m1++)
-				{
-					for (m2 = 0; m2 <= 9; m2++)
-					{
-						_putchar(h1 + '0');
-						_putchar(h2 + '0');
-						_putchar(':');
-						_putchar(m1 + '0');
-						_putchar(m2 + '0');
-					}
-				}
-			}
-		}
+	       	i++;
 	}
 }
