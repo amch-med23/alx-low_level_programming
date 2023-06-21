4dc6f99a980358e45d0062f4b2cf02eca1b24169
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 22:08:50 +0000

    separating the printing of the last 7 fibonacci numbers form the first 91, and using a deferant technic to calculate and print them

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,41 @@ int main(void)
 {
 	unsigned long int sum, prev1, prev2;
 	unsigned long int counter;
+	unsigned long fib1_1, fib1_2, fin2_1, fib2_2;
+	unsigned long v1, v2;
 
 	prev1 = 1;
 	prev2 = 0;
-	for (counter = 1; counter <= 98 ; counter++)
+	for (counter = 0; counter <= 92 ; counter++)
 	{
 		sum = prev1 + prev2;
 		printf("%lu, ", sum);
-		if (counter == 98)
-		{
-			printf("%lu\n", sum);
-		}
 		prev2 = prev1;
 		prev1 = sum;
 	}
+	fib1_1 = prev1 / MAX_VALUE;
+	fib2_1 = prev2 / MAX_VALUE;
+	fib1_2 = prev1 % MAX_VALUE;
+	fib2_2 = prev2 % MAX_VALUE;
+	for (counter = 93; counter <= 99; counter++)
+	{
+		v1 = fib1_1 + fib2_1;
+		v2 = fib1_2 + fib2_2;
+		if (v2 <= MAX_VALUE)
+		{
+			v1 += 1;
+			v2 %= MAX_VALUE;
+		}
+		printf("%lu%lu", v1, v2);
+		if (counter != 98)
+		{
+			printf(", ");
+		}
+		fib1_1 = fib2_1;
+		fib1_2 = fib2_2;
+		fib2_1 = v1;
+		fib2_2 = v2;
+	}
+	printf("\n");
 	return (0);
 }
