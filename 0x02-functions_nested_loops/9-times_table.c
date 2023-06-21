d6f93e19291935b1c37235d61393be5c8f7fe2f5
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 12:37:25 +0000

    reducing the number of columns and lines by 1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	i = 0;
-	while (i <= 10)
+	while (i < 10)
 	{
 		k = 0;
 		res = 0;
-		while (k <= 10)
+		while (k < 10)
 		{
 			res = arr[i] * arr[k];
 			if (k <= 9 && i <= 9)
