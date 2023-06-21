4cccbc5151727e978b0e16c0eccfc8be5590ee72
amch-med23 <skyied.med2023@gmail.com>
2023-06-21 14:08:43 +0000

    adding the defenition of the parameters i and j, and the description for the return value

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
  * add - adds the two integers.
+ * @i: the first operand.
+ * @j: the second operand.
  *
+ * Return: Returns the value of the addition.
  */
 
 int add(int i, int j)
