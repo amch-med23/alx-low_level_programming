8fa45dbe62df8013004adc893eedec9d320376f9
amch-med23 <skyied.med2023@gmail.com>
2023-06-22 18:56:24 +0000

    adding description for the return value

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
-
 /**
- * _isupper - returns 1 if the parameter Uppercase and
- * 0 it it is lowercase.
+ * _isupper - returns 1 if the parameter is Uppercase and
+ * 0 if it is lowercase.
  * @c: the operand.
+ *
+ * Return: (1) if the operand is uppercase and (0) if it's lowercase.
  */
 
 int _isupper(int c)
@@ -11,7 +12,8 @@ int _isupper(int c)
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
-	} else if (c >= 97 && c <= 122)
+	}
+	else if (c >= 97 && c <= 122)
 	{
 		return (0);
 	}
