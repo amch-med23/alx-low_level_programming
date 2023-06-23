86ed5847d896ef556808aa555a25efaf33063fa8
amch-med23 <skyied.med2023@gmail.com>
2023-06-23 12:51:23 +0000

    changing the code format and using 1B as MAX_VALUE value instead of 10B, as well as changing the printf() positions

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX_VALUE 10000000000
+#define MAX_VALUE 1000000000
 /**
  * main - the Entry point.
  * Return: it returns (0), Success.
@@ -7,42 +7,32 @@
 
 int main(void)
 {
-	unsigned long sum, prev1, prev2;
-	int counter;
-	unsigned long fib1_1, fib1_2, fib2_1, fib2_2;
-	unsigned long v1, v2;
+	unsigned long int sum, prev1, prev2;
+	unsigned long int counter;
+	unsigned long int fib1_1, fib1_2, fib2_1, fib2_2;
+	unsigned long int v1, v2;
 
-	prev1 = 0;
-	prev2 = 1;
-	for (counter = 0; counter <= 92 ; counter++)
+	prev1 = 1;
+	prev2 = 2;
+	printf("%lu", prev1);
+	for (counter = 0; counter < 91 ; counter++)
 	{
-		sum = prev1 + prev2;
-		printf("%lu, ", sum);
-		prev1 = prev2;
-		prev2 = sum;
+		printf(", %lu", prev2);
+		prev2 += prev1;
+		prev1 = prev2 - prev1;
 	}
 	fib1_1 = prev1 / MAX_VALUE;
 	fib2_1 = prev2 / MAX_VALUE;
 	fib1_2 = prev1 % MAX_VALUE;
 	fib2_2 = prev2 % MAX_VALUE;
-	for (counter = 93; counter < 99; counter++)
+	for (counter = 92; counter < 99; counter++)
 	{
-		v1 = fib1_1 + fib2_1;
-		v2 = fib1_2 + fib2_2;
-		if (fib1_2 + fib2_2 >= MAX_VALUE)
-		{
-			v1 += 1;
-			v2 %= MAX_VALUE;
-		}
-		printf("%lu%lu", v1, v2);
-		if (counter != 98)
-		{
-			printf(", ");
-		}
-		fib1_1 = fib2_1;
-		fib1_2 = fib2_2;
-		fib2_1 = v1;
-		fib2_2 = v2;
+		printf(", %lu", fib1_2 + (fib2_2 / MAX_VALUE));
+		printf("%lu", fib2_2 % MAX_VALUE);
+		fib2_1 = fib2_1 + fib1_1;
+		fib1_1 = fib2_1 - fib1_1;
+		fib2_2 = fib2_2 + fib1_2;
+		fib1_2 = fib2_2 + fib1_2;
 	}
 	printf("\n");
 	return (0);
