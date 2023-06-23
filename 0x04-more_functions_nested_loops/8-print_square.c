bfcfe23e86fb2fa03766e2bfafb58eb73d2c2965
amch-med23 <skyied.med2023@gmail.com>
2023-06-23 02:51:37 +0000

    adding the file 8-print_square.c that contains the definition of the function print_square()

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square -  printing a square.
+ * @size: the size of the square.
+ * 
+ * Return: it returns nothing.
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		i = 1;
+		while(i <= size)
+		{
+			j = 1;
+			while (j <= size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
