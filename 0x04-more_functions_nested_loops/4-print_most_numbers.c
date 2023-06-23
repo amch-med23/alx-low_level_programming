876f231c2b5cbe186331149d1d78de133d50bfc8
amch-med23 <skyied.med2023@gmail.com>
2023-06-23 02:28:26 +0000

    adding the file 4-print_most_numbers.c that contains the definition of the function print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers fron 0 - 9
+ * excluding 2 and 4.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 58)
+	{
+		if ( i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
