4272df9cf0557db8737ea0ed9b1565996f31e1ec
amch-med23 <skyied.med2023@gmail.com>
2023-06-23 02:41:14 +0000

    adding the file 6-print_line.c that contains the definition of the function print_line()

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints stright line with a width.
+ * specified by the parameter.
+ * @n: the width of the line.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{	
+		i = 1;
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
