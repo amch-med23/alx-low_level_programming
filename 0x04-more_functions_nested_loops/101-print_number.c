bbb5c38aa62c99c5d8e98726648c865f92daff37
amch-med23 <skyied.med2023@gmail.com>
2023-06-23 17:51:15 +0000

    redoing the task using recursion method (calling a function from it self)

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,21 @@
 
 void print_number(int n)
 {
+	unsigned int i;
+
 	if (n < 0)
 	{
-		n = -n;
+		i = -n;
 		_putchar('-');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	} else if (n < 10 || n == 0)
-	{
-		_putchar(n);
-	} else if (n >= 10 && n < 100)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	} else if (n >= 100 && n < 1000)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	} else if (n >= 1000 && n < 10000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
 	}
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
