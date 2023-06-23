5d64924bda0177ac53daa1f4a2ca98f0860da063
amch-med23 <skyied.med2023@gmail.com>
2023-06-23 02:26:02 +0000

    adding the file main.h that conains the prototype of all the funtions, and the file 2-mul.c that defines the function mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - prints the multiplication of two passed numbers.
+ * @a: the first number.
+ * @b: the second number
+ *
+ * Return: returns the result as an integer.
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
