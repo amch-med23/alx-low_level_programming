ed1e34aa5378b14b40b49cebcf67ee5b54cca620
amch-med23 <skyied.med2023@gmail.com>
2023-06-26 23:14:16 +0000

    addng the prototype of the function swap_int() to main.h, and creating the file 1-swap.c that contain the definition of that function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers.
+ * @a: the first integer (refered by address).
+ * @b: the second integer (refered by address).
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	*a = temp;
+	temp = *b;
+	*b = temp;
+}
