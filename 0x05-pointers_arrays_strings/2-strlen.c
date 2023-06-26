9e11e5cdb560de51e4267ef2694711f3768655a9
amch-med23 <skyied.med2023@gmail.com>
2023-06-26 23:29:57 +0000

    adding the function prototype to main.h, and creating the file 2-strlen.v that contains the definition of the above function.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - defines the length of a string.
+ * @s: the string that we want to calculate the longht to.
+ */
+
+int _strlen(char *s)
+{
+	int size_of_string;
+	int size_of_int;
+	int total_length;
+
+	size_of_string = sizeof(s);
+	size_of_int = sizeof(int);
+	total_length = size_of_string / size_of_length;
+	return (total_length);
+}
