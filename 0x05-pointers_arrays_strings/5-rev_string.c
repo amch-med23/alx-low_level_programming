4dea2328685af85e4bd02abdfcce6a23d19a2a30
amch-med23 <skyied.med2023@gmail.com>
2023-06-27 02:16:43 +0000

    changing the method used and using the for loop

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,25 +7,20 @@
 void rev_string(char *s)
 {
 	int i, string_length, k;
+	char arr = s[0];
 
 	i = 0;
-	string_length = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	string_length = i - 1;
 	k = 0;
-	while (string_length >= 0)
+	while ( k < i)
 	{
-		if (string_length == 0)
-		{
-			s[k] = s[0];
-			_putchar('\0');
-			break;
-		}
-		s[k] = s[string_length];
+		í--;
+		arr = s[k];
+		s[k] = s[i];
+		s[i] = arr;
 		k++;
-		string_length--;
 	}
 }
