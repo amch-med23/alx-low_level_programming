9cda8c55f85b36eab2e246bd7181a32dec505d5b
amch-med23 <skyied.med2023@gmail.com>
2023-06-27 02:50:26 +0000

    adding the function prototype to main.h, and creating the file 9 that contains the function definition

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pinted to by src to.
+ * dest.
+ * @src: the source chars.
+ * @dest: the destination chars.
+ *
+ * Return: it returns the dest char.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int c;
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[a] = src[a];
+	}
+	dest[c] = '\0';
+	return (dest);
+}
