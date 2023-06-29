90e62b8b71b78f57474633dbf445de6838d39974
amch-med23 <skyied.med2023@gmail.com>
2023-06-29 21:08:10 +0000

    chanig the code to using increments toget trough the elements of dest and src

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,22 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i, j;
 	char *temp;
 
-	temp = dest - '\0';
-	dest = temp + *src;
-	temp = dest + '\0';
-	dest = temp;
-	return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	temp = dest;
+	return (temp);
 }
