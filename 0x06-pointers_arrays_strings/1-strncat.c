778622c30c86e640b9c5611d3b54f0566b3557df
amch-med23 <skyied.med2023@gmail.com>
2023-06-29 21:21:41 +0000

    creating the file 1 that contains the definition of the function _strncat() and adding that functions prototype to main.h

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings with
+ * the selection of the n bytes from the second string.
+ * @des: the destination.
+ * @src: the source.
+ * @n: the number of bytes it's gonna take from the source.
+ *
+ * Return: it returns a string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *temp;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	temp = dest;
+	return (temp);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 int _putchar(int c);
 char *_strcat(char *desk, char *src);
+char *_strncat(char *dest, char *src, int n);
