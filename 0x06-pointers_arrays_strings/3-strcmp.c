00f98030c6f3fc6dfca0eec04926946785ce743c
amch-med23 <skyied.med2023@gmail.com>
2023-06-29 22:14:22 +0000

    removing the old code and returning the difference between the differant byte.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,23 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int cast1, cast2, i, j;
 	int res;
 
 	i = 0;
-	cast1 = 0;
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		cast1 += (int) s1[i];
+		if (s1[1] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	cast2 = 0;
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		cast2 += (int) s2[j];
-		j++;
-	}
-	res = cast1 - cast2;
-	return (res);
+	return (¨0);
 }
