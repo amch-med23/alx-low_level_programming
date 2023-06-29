e5ebdb290f0db1c92a851498aa05f15432781e66
amch-med23 <skyied.med2023@gmail.com>
2023-06-29 21:49:53 +0000

    creating the file 2-strncpy, that containf the definition of the function _strncpy(), and adding the prototype of this function to the file main.h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpr - copies the string from src to dest
+ * in range of (n) bytes, if the src is less than
+ * the n bytes the dest remaining bytes must hold
+ * the value of '\0' to reach the full lenght of n bytes.
+ *
+ * @dest: the destination.
+ * @src: the source string.
+ * @n: the number of bytes to copy.
+ *
+ * Return: it's returning the sinal string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	char *temp;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	temp = dest;
+	return (temp);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 int _putchar(int c);
 char *_strcat(char *desk, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
