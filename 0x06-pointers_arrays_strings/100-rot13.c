498578a8c3d27e445b4b18a4bf23400e39714e53
amch-med23 <skyied.med2023@gmail.com>
2023-07-01 00:27:41 +0000

    advanced task 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the entry point.
+ *
+ * Return: it returns (0), means Success.
+ */
+int main(void)
+{
+	char v[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+	char *g;
+
+	g = rot13(s);
+	printf("%s", g);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	printf("------------------------------------\n");
+	g = rot13(v);
+	printf("%s", g);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	printf("------------------------------------\n");
+	g = rot13(v);
+	printf("%s", g);
+	printf("------------------------------------\n");
+	printf("%s", v);
+	return (0);
+}
