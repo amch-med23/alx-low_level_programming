52b43a2c308862d037d7c33fe182f2b800e0ea4f
amch-med23 <skyied.med2023@gmail.com>
2023-07-01 00:33:31 +0000

    chanig the code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,28 +2,29 @@
 #include <stdio.h>
 
 /**
- * main - the entry point.
+ * rot13 - encodes in rot13 algo.
+ * @s: ponits to the string.
  *
  * Return: it returns (0), means Success.
  */
-int main(void)
-{
-	char v[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-	char *g;
 
-	g = rot13(v);
-	printf("%s", g);
-	printf("------------------------------------\n");
-	printf("%s", v);
-	printf("------------------------------------\n");
-	g = rot13(v);
-	printf("%s", g);
-	printf("------------------------------------\n");
-	printf("%s", v);
-	printf("------------------------------------\n");
-	g = rot13(v);
-	printf("%s", g);
-	printf("------------------------------------\n");
-	printf("%s", v);
-	return (0);
+char rot13(char *s)
+{
+	int i;
+	int j;
+	char d1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char drot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == d1[j])
+			{
+				s[i] = drot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
