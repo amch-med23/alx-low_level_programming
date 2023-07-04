470d223c2d03bce4ec0489918a108585a8ab58a5
amch-med23 <skyied.med2023@gmail.com>
2023-07-04 01:09:10 +0000

    removing thriling whitespaces

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int j;
-	
-        j = 0;
-        while (j < n)
-        {
-                *((char *) dest + j) = *((char *) src + j);
-                j++;
-        }
-	
-        return (dest);
+	unsigned int j;
+
+	j = 0;
+	while (j < n)
+	{
+		*((char *) dest + j) = *((char *) src + j);
+		j++;
+	}
+	return (dest);
 }
