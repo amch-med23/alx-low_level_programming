d4accdcc15fceeb68bfe9026b2b8a6a60adfd3ce
amch-med23 <skyied.med2023@gmail.com>
2023-07-05 21:44:14 +0000

    creating the file 5 that contains the definition of the function _sqrt_recursion()

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+int vol_sqrt_recursion(int n, int j);
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of.
+ *
+ * Return: the resulting square root of the given number.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (vol_sqrt_recursion(n, 0));
+}
+
+/**
+ * vol_sqrt_recursion - recurses to find the natural
+ * square root of a given number.
+ * @n: the number to calculate the sqaure root of.
+ * @i: the iterator
+ *
+ * Return: the resulting square root
+ */
+int vol_sqrt_recursion(int n, int j)
+{
+	if (j * j > n)
+	{
+		return (-1);
+	}
+	if (j * j == n)
+	{
+		return (j);
+	}
+	return (vol_sqrt_recursion(n, j + 1));
+}
