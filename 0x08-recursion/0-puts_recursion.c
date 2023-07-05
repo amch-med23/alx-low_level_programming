cfe29d89503d169bdb59a25516f2b1ebecef89e3
amch-med23 <skyied.med2023@gmail.com>
2023-07-05 20:02:45 +0000

    changing the operation used into arrays

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,11 @@ void _puts_recursion(char *s)
 	int i;
 
 	i = 0;
-	if (*(s + i) == '\0')
+	if (s[i] == '\0')
 	{
 		_putchar('\n');
 		return (0);
 	}
-	_putchar(_puts_recursion(*(s + i)));
+	_putchar(_puts_recursion(s[i]);
 	i++;
 }
