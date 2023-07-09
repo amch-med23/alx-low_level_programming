605339463ac5faf5951d29d5650cb4126bca6715
amch-med23 <skyied.med2023@gmail.com>
2023-07-09 21:49:46 +0000

    adding everything to the remote repo

---
+++ 0x09-static_libraries/#3-puts.c#
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i += 1;
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks if a character is upper-case or not.
+ * @c: the character.
+ *
+ * Return: an integer.
+ */
+int _isupper(int c)
+{
+}
