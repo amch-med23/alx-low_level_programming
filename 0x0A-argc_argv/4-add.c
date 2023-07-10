433b1b11bbc48dbcf384369fdd9aed7bd54d3282
amch-med23 <skyied.med2023@gmail.com>
2023-07-10 18:08:28 +0000

    adding the file 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers give as arguments.
+ * @argc: the argument counter.
+ * @argv: the argument vector.
+ *
+ * Return: (1) or EXIT_SUCCESS.
+ */
+
+int main(int argc, char *argv[])
+{
+	int argnum;
+	int i, zero;
+	int finalres;
+	
+	i = 1;
+	zero = 0;
+	argnum = argc - 1;
+	finalres = 0;
+	if (argnum == 0)
+	{
+		printf("%d\n", zero);
+		exit(EXIT_SUCCESS);
+	}
+	while (i <= argnum)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	i = 1;
+	finalres = 0;
+	while (i <= argnum)
+	{
+		if (atoi(argv[i]) > 0)
+		{
+			finalres += atoi(argv[i]); 
+		}
+		i++;
+	}
+	printf("%d\n", finalres);
+	exit(EXIT_SUCCESS);
+}
