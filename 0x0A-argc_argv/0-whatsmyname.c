92517b8ef2196c2aaee9ea06670ac6405524e0e0
amch-med23 <skyied.med2023@gmail.com>
2023-07-10 00:48:28 +0000

    creating the project 0x0A folder, and adding the first file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point.
+ * @argc: the argument counter.
+ * @argv: the argument vector(array of strings).
+ *
+ * Return: it returns Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
