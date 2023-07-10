d5df1f3331dc90b01307649c76bc6e7014f4e328
amch-med23 <skyied.med2023@gmail.com>
2023-07-10 00:55:34 +0000

    adding the file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point.
+ * @argc: the argument counter.
+ * @argv: the argument vector.
+ *
+ * Return: it returns success.
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc-1);
+}
