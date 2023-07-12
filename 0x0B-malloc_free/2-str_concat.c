c065193f2d11b8c5f21cbc5917a9b2fa54adaa5c
amch-med23 <skyied.med2023@gmail.com>
2023-07-12 22:15:46 +0000

    using another method of assigning the value to dest.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,7 +5,7 @@
  * str_concat - concatenates two given strings.
  * @s1: the first string.
  * @s2: the second string.
- * 
+ *
  * Return: return a new ptr to the newelly created
  * space in memory.
  */
@@ -13,8 +13,8 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned long int totalsize, sizeS1, sizeS2;
 	char *dest;
-	unsigned long int i, j, k;
-	
+	unsigned long int i;
+
 	sizeS1 = (int)strlen(s1);
 	sizeS2 = (int)strlen(s2);
 	totalsize = sizeS1 + sizeS2;
@@ -27,31 +27,16 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	i = 0;
-	while (i < sizeS1 && s1 != NULL)
+	for (i = 0; i < sizeS1 + sizeS2; i++)
 	{
-		if (s1[i] != '\0')
+		if (i < sizeS1)
 		{
 			dest[i] = s1[i];
-			i++;
 		}
-	}
-	j = 0;
-	while (j < sizeS2 && s2 != NULL)
-	{
-		if (j == sizeS2)
-			dest[sizeS1 + j] = '\0';
-		dest[sizeS1 + j] = s2[j];
-		j++;
-	}
-	if (s2 == NULL || s1 == NULL)
-	{
-		k = 0;
-		while (k <= sizeof(s1) || k <= sizeof(s2))
+		else
 		{
-			dest[k] = s1[k] + s2[k];
-			k++;
-		}  
+			dest[i] = s2[i - sizeS1];
+		}
 	}
 	return (dest);
 }
