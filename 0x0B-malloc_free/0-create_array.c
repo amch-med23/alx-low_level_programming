ea5f01dc9cbad1e08bdf64e3e631088158358f97
amch-med23 <skyied.med2023@gmail.com>
2023-07-12 00:26:00 +0000

    adding the definition of the ptr s, and adding the betty documenting style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array - 
- *
- *
- *
+ * create_array - creates an array and returning it.
+ * @size: the size of the aray.
+ * @c: the character.
+ * Return: it returns a pointer to the string
  */
 
-char * create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	int i;
+	char *s;
 
 	i = 0;
 	s = malloc(sizeof(*s) * size);
