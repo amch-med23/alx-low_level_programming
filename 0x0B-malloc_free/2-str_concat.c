3bf62574a551f4307cb26fa5737b10cd2b4fe11c
amch-med23 <skyied.med2023@gmail.com>
2023-07-12 22:22:15 +0000

    incrementing the totalsize by 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,10 +26,10 @@ char *str_concat(char *s1, char *s2)
 		sizeS2++;
 	}
 	totalsize = sizeS1 + sizeS2;
-	dest = malloc(sizeof(char) * totalsize);
+	dest = malloc(sizeof(char) * totalsize + 1);
 	if (dest == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < totalsize; i++)
 	{
