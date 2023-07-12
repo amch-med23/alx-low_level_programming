c44b6a86a675c4c802f1cfd554b9d077a0c7af26
amch-med23 <skyied.med2023@gmail.com>
2023-07-12 01:18:53 +0000

    adding the file 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strdup - gives a ptr to a duplicated string in memory.
+ * @str: the string to doplicate.
+ *
+ * Return: a ptr to the memory address of the newely created
+ * string.
+ */
+
+char *_strdup(char *str)
+{
+	char *dest;
+	int size, i __attribute__ ((unused));
+
+	if (str == NULL )
+	{
+		return (NULL);
+	}
+	size = strlen(str);
+	dest = malloc(sizeof(char) * size);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i <= size)
+	{
+		*(dest + i) = *(str + i);
+		i++;
+	} 
+	return dest;
+}
