0e1627fafad38f002f102532b43ddd5250c6218e
amch-med23 <skyied.med2023@gmail.com>
2023-07-13 20:04:47 +0000

    fixing betty-styling errors

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,9 +9,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	unsigned int  *j;
-	j = malloc((int)b);
+	unsigned int *j;
+
+	j = malloc((int) b);
 	if (j == NULL)
-		exit (98);
+		exit(98);
 	return (j);
 }
