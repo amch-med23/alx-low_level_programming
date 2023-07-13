845bac4521f37934093ab8e431767834f3a7ebac
amch-med23 <skyied.med2023@gmail.com>
2023-07-13 20:02:17 +0000

    adding the folder 0x0C and the main.h, README.md, 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: the size to allocate.
+ *
+ * Return: a pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int  *j;
+	j = malloc((int)b);
+	if (j == NULL)
+		exit (98);
+	return (j);
+}
