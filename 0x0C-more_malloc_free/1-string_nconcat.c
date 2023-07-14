276c30f8d25c92bc5e1dde76a918024586c215fd
amch-med23 <skyied.med2023@gmail.com>
2023-07-14 17:18:27 +0000

    adding missing ++ to the 'j' counter in the last while loop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	while (n >= sizeS2 && i < (sizeS1 + sizeS2))
 	{
-		dest[i++] = s2[j];
+		dest[i++] = s2[j++];
 	}
 	dest[i] = '\0';
 	return (dest);
