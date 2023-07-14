20fdbb63bc1c4ebcbcbbdede8531ff9896ec189b
amch-med23 <skyied.med2023@gmail.com>
2023-07-14 19:09:49 +0000

    fixing betty-styling error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min;
-	dest = malloc(sizeof(int) * size + 1);
+	dest = malloc(sizeof(int) * size++);
 	if (!dest)
 		return (NULL);
 	i = min;
