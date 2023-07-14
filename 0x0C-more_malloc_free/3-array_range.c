55f2263af5bae98ab85bf11b9a5344110b1de702
amch-med23 <skyied.med2023@gmail.com>
2023-07-14 19:08:22 +0000

    fixing betty-style error, and adding 1 to the size in malloc()

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min;
-	dest = malloc((size + 1) * sizeof(int));
+	dest = malloc(sizeof(int) * size + 1);
 	if (!dest)
 		return (NULL);
 	i = min;
