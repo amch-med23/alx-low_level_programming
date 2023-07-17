d62a053ce1b8314ec9e658c94c9cb5df5cb9d63c
amch-med23 <skyied.med2023@gmail.com>
2023-07-17 20:22:30 +0000

    adding the file 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point.
+ *
+ * Return: EXIT_SUCCESS.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	exit (EXIT_SUCCESS);
+}
