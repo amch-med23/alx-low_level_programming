d377858984af20003004b4795b279e61a53bf3e6
amch-med23 <skyied.med2023@gmail.com>
2023-07-18 00:58:53 +0000

    fixing a typo in the word (nil)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,8 +18,8 @@ void print_dog(struct dog *d)
 	else if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: (nill)\n");
+	if (d->age <= 0)
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
