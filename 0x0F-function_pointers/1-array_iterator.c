401b58308569400d64627f09e18b4d2e966825e0
amch-med23 <skyied.med2023@gmail.com>
2023-07-19 20:38:03 +0000

    adding the files for task 2, and updating the header file function_pointers.h

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - iterates through elements of
+ * a given pointer to array.
+ * @array: a pointer to the array.
+ * @size: tha array size.
+ * @action: the action function to use (this means
+ * what to do with the elements of the given array).
+ *
+ * Return: nothing (void).
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int element;
+	
+	for (element = 0; element < size; element++)
+	{
+		action(array[element]);
+	}
+}
