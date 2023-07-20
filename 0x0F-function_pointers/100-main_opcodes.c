b736992c98830495dcdc59c967b10a2fac36649e
amch-med23 <skyied.med2023@gmail.com>
2023-07-20 00:19:02 +0000

    adding the file 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints opcodes of its own.
+ * @argc: the argument counter.
+ * @argv: the argument vectore.
+ *
+ * Return: it returns EXIT_SUCCESS.
+ */
+int main(int argc, char *argv[])
+{
+	exit(EXIT_SUCCESS);
+}
