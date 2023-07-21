cd5ff50e0be94f7bbaa00defbb0663e9fe6dc08c
amch-med23 <skyied.med2023@gmail.com>
2023-07-21 18:02:13 +0000

    changing the type of i, and typecasting n to (int)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,18 +13,18 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i;
+	int i;
 	char *string;
 
 	va_start(ap, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int)n; i++)
 	{
 		string = va_arg(ap, char*);
 		if (!(*string))
 			printf("(nil)");
 		else if (string)
 			printf("%s", string);
-		if ((separator) && i < (int)(n - 1))
+		if ((separator) && (i != (int)n - 1))
 			printf("%s", separator);
 	}
 	va_end(ap);
