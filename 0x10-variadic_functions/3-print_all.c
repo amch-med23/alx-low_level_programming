d6062a3696417c945d8bcd67c757fde775355277
amch-med23 <skyied.med2023@gmail.com>
2023-07-21 18:17:22 +0000

    changing the code to use another way of implementing the separator

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,7 @@
 /**
  * print_all - a function that prints a given strings,
  * as arguments, via a supplied format list of types.
- * @foramt: the format list of types.
+ * @format: the format list of types.
  *
  * Return: nothing (void).
  */
@@ -13,46 +13,40 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	const char *ptr;
-	char *string;
+	char *string, *sep = "";
 	int i;
-	int bl;
-	
+
 	i = 0;
-	ptr = format;
 	va_start(ap, format);
-	while (ptr[i] != '\0')
+
+	if (format)
 	{
-		bl = 0;
-		switch (ptr[i])
+		while (format[i])
 		{
-		case 'i':
-			printf("%d", va_arg(ap, int));
-			bl = 1;
-			break;
-		case 's':
-			string = va_arg(ap, char *);
-			if (!(*string))
+			switch (format[i])
 			{
-				printf("(nil)");
-				bl = 1;
-				break;
+				case 'c':
+					printf("%s%c", sep, va_arg(ap, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(ap, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(ap, double));
+					break;
+				case 's':
+					string = va_arg(ap, char *);
+					if (!string)
+						string = "(nil)";
+					printf("%s%s", sep, string);
+					break;
+				default:
+					i++;
+					continue;
 			}
-			printf("%s", string);
-			bl = 1;
-			break;
-		case 'c':
-			printf("%c", (char) (va_arg(ap, int)));
-			bl = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(ap, double));
-			bl = 1;
-			break;
+			sep = ", ";
+			i++;
 		}
-		if (ptr[i + 1] != '\0' && bl)
-			printf(", ");
-		i++;
 	}
 	va_end(ap);
 	printf("\n");
