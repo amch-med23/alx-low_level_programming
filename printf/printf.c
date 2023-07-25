323ad7d8cf88ba2cb466941424e1393e7a591000
amch-med23 <skyied.med2023@gmail.com>
2023-07-25 12:40:57 +0000

    adding the first files

---
+++ printf/printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _printf - this is a similar function to
+ * the printf standard function.
+ * @format: the format specifier.
+ * @...: the elipsis indicating unspecified
+ * argument numer and type.
+ *
+ * Return: the number of the character printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
