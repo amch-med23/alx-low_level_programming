54b8db4d5f72696a1250a740e490086c8be3f4a7
amch-med23 <skyied.med2023@gmail.com>
2023-08-01 21:28:39 +0000

    adding the file 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+* free_listint2 - frees a memory allocated to a linked list
+* @head: pointer to the head of the linked list.
+*
+* Return: nothing (void)
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	const listint_t *node;
+
+	node = *head;
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (node != NULL)
+	{
+		temp = node->next;
+		free(node);
+		node = temp;
+	}
+
+	head = NULL;
+}
