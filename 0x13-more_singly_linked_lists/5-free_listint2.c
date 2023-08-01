071a1258323d102a7f3fa186b45f7b7169fbb82f
amch-med23 <skyied.med2023@gmail.com>
2023-08-01 21:36:02 +0000

    fixing betty-styling issues

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *node __attribute__((unused));
 
 	if (head == NULL)
 	{
 		return;
 	}
-
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-
 	head = NULL;
 }
