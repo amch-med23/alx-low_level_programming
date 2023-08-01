9e50ae07343a217a6cb9b95dd8b700cee5bdab17
amch-med23 <skyied.med2023@gmail.com>
2023-08-01 21:43:17 +0000

    returniing the pointer 'ptr' instead of d

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,16 +10,17 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *ptr;
-	unsigned int d = 0;
+	unsigned int d;
 
 	ptr = NULL;
+	d = 0;
 	while (head != NULL)
 	{
 		if (d <= index)
 		{
 			if (d == index)
 			{
-				ptr =  head;
+				ptr = head;
 				break;
 			}
 			head = head->next;
@@ -30,5 +31,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			return (NULL);
 		}
 	}
-	return (d);
+	return (ptr);
 }
