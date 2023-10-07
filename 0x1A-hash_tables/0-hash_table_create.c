872cb627bc42debafa81f5e57aa73b1b4923225d
amch-med23 <email@example.com>
2023-10-07 02:42:01 +0100

    changing the name of the ht referer to hash_tale

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,22 +9,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *hash_table;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	ht->size = size;
+	hash_table->size = size;
 	/* this is to handle the chaining collusion */
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 		return (NULL);
 	i = 0;
 	while (i < size)
 	{
-		ht->array[i] == NULL;
+		hash_table->array[i] == NULL;
 		i++;
 	}
-	return (ht);
+	return (hash_table);
 }
